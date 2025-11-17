1356bcbb0b5709674e527a55149388619fade8ba
azharm1224 <azharmaulana533487@mail.ugm.ac.id>
2025-11-17 08:42:58 +0700

    On branch main modified:   src/preprocessing/preprocess.py

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -4,3 +4,16 @@ import numpy as np
 def preprocess_image(image_path:str) -> np.ndarray:
     # baca gambar grayscale 
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    if img is None: 
+        raise ValueError(f"Gambar tidak dapat dibaca: {image_path}")
+    
+    # gaussian blur untuk mengurangi noise
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+
+    # CLAHE untuk meningkatkan kontras
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    img = clahe.apply(img)
+
+    # output berupa array (HxW)
+    return img
